6a51103099faba828c391c61265a11488a25ae7c
wcdaren <wcdaren@gmail.com>
2019-07-08 18:54:44 +0800

    初始化

--- src/index.js
+++ src/index.js
@@ -1,12 +1,55 @@
+/*
+/!*BASE*!/
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import ReactDOM, {render} from 'react-dom';
+import {HashRouter, Switch, Route, Redirect} from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+/!*REDUX STORE*!/
+import {Provider} from 'react-redux';
+import store from './store/index';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+/!*ANTD*!/
+import {LocaleProvider} from 'antd';
+import zh_CN from 'antd/lib/locale-provider/zh_CN';
+
+/!*IMPORT CSS*!/
+import './static/css/reset.min.css';
+import './static/css/common.less';
+
+/!*IMPORT COMPONENT*!/
+import NavTop from './component/NavTop';
+import NavBottom from './component/NavBottom';
+import Home from './routes/Home';
+import Mycourse from './routes/Mycourse';
+import Person from './routes/Person';
+
+/!*RENDER*!/
+render(<Provider store={store}>
+    <HashRouter>
+        <LocaleProvider locale={zh_CN}>
+            <div>
+                {/!*HEADER*!/}
+                <NavTop/>
+
+                {/!*MAIN=>ROUTE*!/}
+                <main className='container'>
+                    <Switch>
+                        <Route path='/course' component={Home}/>
+                        <Route path='/mycourse' component={Mycourse}/>
+                        <Route path='/person' component={Person}/>
+                        <Redirect to='/course'/>
+                    </Switch>
+                </main>
+
+                {/!*FOOTER*!/}
+                <NavBottom/>
+            </div>
+        </LocaleProvider>
+    </HashRouter>
+</Provider>, root);*/
+
+import axios from 'axios';
+
+axios.get('/message/allCount').then(result => {
+    console.log(result);
+});
