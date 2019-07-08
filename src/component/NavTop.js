6bde3d5549b56e15a57897318daa601fd85fa186
wcdaren <wcdaren@gmail.com>
2019-07-08 23:41:26 +0800

    topnav

--- src/component/NavTop.js
+++ src/component/NavTop.js
@@ -1,30 +1,69 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-import { connect } from 'react-redux'
-import { withRouter } from 'react-router-dom'
-import { Icon } from 'antd'
+import React from 'react';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+import { Icon } from 'antd';
+import action from '../store/action/index';
 
-export class NavTop extends Component {
+/*TRANSITION*/
+import Transition from 'react-transition-group/Transition';
+
+const duration = 300,
+  defaultStyle = {
+    transition: `opacity ${duration}ms`,
+    opacity: 0
+  }, transitionStyles = {
+    entering: { opacity: 0 },
+    entered: { opacity: 1 }
+  };
+
+class NavTop extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      in: false
+    };
+  }
+
+  handleClick = ev => {
+    let target = ev.target,
+      tarTag = target.tagName;
+    if (tarTag === 'LI') {
+      this.setState({ in: false });
+    }
+  };
 
   render() {
     return <header className='headerNavBox'>
-      {/* 首页的导航 */}
-      <div className="homeBox">
-        <div className="baseBox">
+      {/*首页的导航*/}
+      <div className='homeBox'>
+        <div className='baseBox'>
           <h1 className='logo'>wcdaren</h1>
+
           <Icon className='icon' type='bars' style={{
-            fontSize: '.8rem'
-          }}></Icon>
+            fontSize: '.6rem'
+          }} onClick={ev => {
+            this.setState({
+              in: !this.state.in
+            });
+          }} />
         </div>
-        <ul className="filterBox">
-          <li>全部内容</li>
-          <li>js</li>
-          <li>react</li>
-          <li>计算机网络</li>
-        </ul>
+        <Transition in={this.state.in} timeout={0}>
+          {state => {
+            return <ul className='filterBox' style={{
+              ...defaultStyle,
+              ...transitionStyles[state],
+              display: this.state.in ? 'block' : 'none'
+            }} onClick={this.handleClick}>
+              <li >全部课程</li>
+              <li >REACT课程</li>
+              <li >VUE课程</li>
+              <li >小程序课程</li>
+            </ul>;
+          }}
+        </Transition>
       </div>
-    </header>
+    </header>;
   }
 }
 
-export default withRouter(connect()(NavTop))
+export default withRouter(connect()(NavTop));
