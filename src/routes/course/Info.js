4dcb7282d3331b1663c552aee218274680033091
wcdaren <wcdaren@gmail.com>
2019-07-15 15:16:43 +0800

    课程闲情

--- src/routes/course/Info.js
+++ src/routes/course/Info.js
@@ -1,27 +1,47 @@
 import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import Qs from 'qs'
 import { connect } from 'react-redux'
+import { Button } from 'antd';
+import { queryInfo } from '../../api/course';
 
 export class Info extends Component {
-    static propTypes = {
-        prop: PropTypes
+  constructor(props, context) {
+    super(props, context)
+    this.state = {
+      data: null
     }
+  }
 
-    render() {
-        return (
-            <div>
-                info
-            </div>
-        )
+  async componentDidMount() {
+    // : 是对属性取另一个名字
+    // : {} 这样就可以形成再次解构
+    let { location: { search } } = this.props
+    let { courseId = 0 } = Qs.parse(search.substr(1)) || {}
+    let result = await queryInfo(courseId)
+
+    if (parseFloat(result.code) === 0) {
+      this.setState({
+        data: result.data
+      })
     }
+  }
+
+  render() {
+    let { data } = this.state
+    if (!data) return ''
+    return (
+      < div className='baseInfo' >
+        <video src="" controls preload='none' poster={data.pic}></video>
+        <div className='content'>
+          <h3>{data.name}</h3>
+          <p>{data.dec}</p>
+          <span>课程价格：{data.price}</span>
+          <Button type='danger'>立即购买</Button>
+        </div>
+      </div >
+    )
+  }
 }
 
-const mapStateToProps = (state) => ({
-    
-})
 
-const mapDispatchToProps = {
-    
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(Info)
+export default Info
